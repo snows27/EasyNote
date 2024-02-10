ae953608fc3fc3c39cbd61ccdb5378baca1a38c3
surze <surzepp@gmail.com>
2024-02-11 05:15:13 +0700

    add footer

--- pages/Signin.vue
+++ pages/Signin.vue
@@ -7,7 +7,7 @@
         <v-text-field
           label="username"
           v-model="username"
-          :rules="usernameRules"     
+          :rules="usernameRules"
         ></v-text-field>
         <span></span>
         <v-text-field
@@ -16,12 +16,10 @@
           v-model="password"
           :rules="passwordRules"
         ></v-text-field>
-        <p v-if="invalidSignin" class="text-red">Please check your username and password</p>
-        <v-btn
-          type="submit"
-          color="primary"
-          class="float-right"
-          @click="signIn"
+        <p v-if="invalidSignin" class="text-red">
+          Please check your username and password
+        </p>
+        <v-btn type="submit" color="primary" class="float-right" @click="signIn"
           ><span class="font-semibold">Sign In</span></v-btn
         >
       </v-form>
@@ -30,55 +28,55 @@
       </nuxt-link>
     </v-sheet>
   </v-container>
+  <Footer />
 </template>
 <script>
-import {defineComponent, ref} from '@vue/composition-api'
+import { defineComponent, ref } from "@vue/composition-api";
 export default defineComponent({
-    async setup() {
-        const route = useRouter()
-        const username = ref(null)
-        const password = ref(null)
-        const dataFetch = await import('../data/data.json')
-        const invalidSignin = ref(false)
-        const users = dataFetch.users
-        
-        const checkUsernameExists = () => {
-          return users.some(user => user.username === username.value)
-        }
-        const checkPassword =() =>{
-          return users.some(user => user.password === password.value)
-        }
-        const usernameRules = [
-             (value) => !!value || 'username is required'   
-        ]
-        const passwordRules = [
-            (value) => !!value || 'password is required'
-        ]
+  async setup() {
+    const route = useRouter();
+    const username = ref(null);
+    const password = ref(null);
+    const dataFetch = await import("../data/data.json");
+    const invalidSignin = ref(false);
+    const users = dataFetch.users;
 
-        const signIn = ()=>{
-            if(username.value !== null || password.value !== null){
-              if(checkUsernameExists() && checkPassword()){
-                alert('login success')
-                const user = users.find(user => user.username === username.value)
-                route.push({path: '/dashboard', query: {userId: user.id, username: user.username}})
-              }
-              else if(username.value === null || password.value === null){
-                invalidSignin.value = false
-              } else {
-                invalidSignin.value = true
-              }
-              
-              }
-              
-            }
-          
-        
-          
-        
-        return {username, password, usernameRules, passwordRules, signIn, invalidSignin}
-   
-    },
-})
+    const checkUsernameExists = () => {
+      return users.some((user) => user.username === username.value);
+    };
+    const checkPassword = () => {
+      return users.some((user) => user.password === password.value);
+    };
+    const usernameRules = [(value) => !!value || "username is required"];
+    const passwordRules = [(value) => !!value || "password is required"];
+
+    const signIn = () => {
+      if (username.value !== null || password.value !== null) {
+        if (checkUsernameExists() && checkPassword()) {
+          alert("login success");
+          const user = users.find((user) => user.username === username.value);
+          route.push({
+            path: "/dashboard",
+            query: { userId: user.id, username: user.username },
+          });
+        } else if (username.value === null || password.value === null) {
+          invalidSignin.value = false;
+        } else {
+          invalidSignin.value = true;
+        }
+      }
+    };
+
+    return {
+      username,
+      password,
+      usernameRules,
+      passwordRules,
+      signIn,
+      invalidSignin,
+    };
+  },
+});
 </script>
 
 
